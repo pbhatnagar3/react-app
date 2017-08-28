0af6627393876c844e7ecfff3453acbd3c87c5aa
Pujun Bhatnagar <pujun@cs.stanford.edu>
2017-08-28 11:02:19 -0700

    simple components and the difference between a stateless and a stateful component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-import React from 'react';
-const App = () => <h1> Hello World </h1> 
+import React from 'react'
 
-export default App;
+// stateful component
+class App extends React.Component {
+  render() {
+    return <h1 classname='Halo'> Halo World </h1>
+
+    // return React.createElement('h1', null, "Hello World");
+    // this is the same as the other thing
+
+  }
+}
+
+// if you don't need the states and would rather just have a simple component
+//  that does not take any input, you can do the following:
+// const App = () => <h1> Hello Stateless </h1>
+export default App
