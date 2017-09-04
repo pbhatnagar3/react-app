f9e71d1737665436bd93ecc8243a85780db98781
Pujun Bhatnagar <pujun@cs.stanford.edu>
2017-09-03 18:16:51 -0700

    lecture 18 complete

--- src/App.js
+++ src/App.js
@@ -2,46 +2,27 @@ import React from 'react';
 import './App.css'
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      err: "",
-      input: "/*Please write the code here*/",
-      output: ""
-    }
-  }
-
-  update(e) {
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel
-        .transform(code, {presets: ['es2015', 'react']})
-        .code, 
-      err: '' 
-      })
-    }
-    catch(err) {
-      this.setState({err: err.message})
-    }
-  }
-
   render() {
     return (
-      <div>
-        <header>{this.state.err}</header>
-        <div className="container">
-          <textarea onChange={this.update.bind(this)} 
-          defaultValue={this.state.input}
-          />
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Parent> 
+        <div className="childA"></div>
+        <div className="childB"></div>
 
+      </Parent>
     )
   }
 }
+
+class Parent extends React.Component {
+  render() {
+    // let items = React.Children.map(this.props.children, child => child);
+    // let items = React.Children.toArray(this.props.children);
+    let items = React.Children.forEach(this.props.children, child => console.log(child.props.className))
+    // console.log(items);
+
+    return null;
+  }
+}
 export default App;
+
+// when there is only item, then you cannot apply map sadly
